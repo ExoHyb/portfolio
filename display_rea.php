8a9eea691368e99451e35016bcc4c3b4c4abf48f
tmartin <theo.martin22@icloud.com>
2016-04-17 21:23:08 +0200

    Lst Md

---
+++ display_rea.php
@@ -0,0 +1,20 @@
+<div class="col-md-3 col-md-offset-3">
+    <div class="global-display">
+        <a href="#">
+            <img src="http://placehold.it/300x300" alt="" />
+            <div class="display-text">
+                <h3 class="display-h3">Example 1</h3>
+            </div>
+        </a>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="global-display">
+        <a href="#">
+            <img src="http://placehold.it/300x300" alt="" />
+            <div class="display-text">
+                <h3 class="display-h3">Example 2</h3>
+            </div>
+        </a>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Portfolio Théo Martin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="style-presentation text-center">
+                        <h1>Théo Martin</h1>
+                        <h2>Web Developer Frontend & Backend</h2>
+                        <h4>22 years old - France</h4>
+                        <div class="skills">
+                            <img class="img-responsive" src="img/skills.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <?php include "display_rea.php"; ?>
+            </div>
+            <div class="row">
+                <?php include "display_rea.php"; ?>
+            </div>
+        </div>
+    </body>
+</html>
